16f287288d10480b72fa288611420697863c8021
saidamst2003 <saida2003mst@gmail.com>
2025-04-10 00:20:21 +0100

    Save changes before updating Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from './about/about.component';
   imports: [NavbarComponent, EcperienceComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
+  
 })
 export class AppComponent {
   title = 'TestApp';
