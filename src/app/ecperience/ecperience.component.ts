56201c62ae2d456cddbcee9f65e348cf6b631133
saidamst2003 <saida2003mst@gmail.com>
2025-04-09 10:35:04 +0100

    j'ai creer les composanr de navbar

---
+++ src/app/ecperience/ecperience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecperience',
+  imports: [],
+  templateUrl: './ecperience.component.html',
+  styleUrl: './ecperience.component.css'
+})
+export class EcperienceComponent {
+
+}
